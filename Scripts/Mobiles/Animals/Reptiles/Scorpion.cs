b848ab1a36e86502965b2e62c4459f738e91ee0d
Simon102978 <mitaniel@hotmail.com>
2024-07-15 20:15:56 -0400

    Modification au Scorpion

--- Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
+++ Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
@@ -1,10 +1,17 @@
+using System;
 using Server.Items;
+using Server;
 
 namespace Server.Mobiles
 {
+    
     [CorpseName("le corps d'un scorpion")]
     public class Scorpion : BaseCreature
     {
+      	public override bool CanStealth => true;
+
+        public DateTime LastFreeze;
+
         [Constructable]
         public Scorpion()
             : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
@@ -35,6 +42,7 @@ namespace Server.Mobiles
             SetSkill(SkillName.MagicResist, 30.1, 35.0);
             SetSkill(SkillName.Tactics, 60.3, 75.0);
             SetSkill(SkillName.Wrestling, 50.3, 65.0);
+            SetSkill(SkillName.Hiding,70.0, 100.0);
 
             Fame = 2000;
             Karma = -2000;
@@ -49,6 +57,91 @@ namespace Server.Mobiles
         {
         }
 
+        public override void OnThink()
+        {
+
+            if (!Alive || Deleted)
+            {
+                return;
+            }
+
+            if (!Hidden)
+            {
+                double chance = 0.10;
+
+                if (Hits < 20)
+                {
+                    chance = 0.1;
+                }
+
+                if (Poisoned)
+                {
+                    chance = 0.01;
+                }
+
+                if (Utility.RandomDouble() < chance)
+                {
+                    HideSelf();
+                }
+                base.OnThink();
+            }
+        }
+
+        public override void OnDamage(int amount, Mobile from, bool willKill)
+        {
+            RevealingAction();
+            base.OnDamage(amount, from, willKill);
+
+        }
+
+
+
+		public override void AlterMeleeDamageTo(Mobile to, ref int damage)
+		{
+             double chance = 0.05;
+
+             if ( 3 < to.GetDistanceToSqrt(this.Location))
+             {
+                chance += 0.20; 
+             }
+
+            if ((Hidden || Utility.RandomDouble() < chance ) && LastFreeze < DateTime.UtcNow )
+            {
+                to.Freeze(TimeSpan.FromSeconds(3));
+                to.Emote("*Le scorpion vous paralyse.*");
+
+                 LastFreeze = DateTime.UtcNow + TimeSpan.FromSeconds(Utility.RandomMinMax(6, 9));
+            }
+
+			
+			base.AlterMeleeDamageTo(to, ref damage);
+		}
+
+
+
+
+        public override void OnDamagedBySpell(Mobile from)
+        {
+            RevealingAction();
+            base.OnDamagedBySpell(from);
+        }
+
+
+        private void HideSelf()
+        {
+            if (Core.TickCount >= NextSkillTime)
+            {
+                Effects.SendLocationParticles(
+                    EffectItem.Create(Location, Map, EffectItem.DefaultDuration), 0x3728, 10, 10, 2023);
+
+                PlaySound(0x22F);
+                Hidden = true;
+
+                UseSkill(SkillName.Hiding);
+            }
+        }
+
+
         public override int Meat => Utility.RandomMinMax(2, 3);
 
 		public override int Hides => Utility.RandomMinMax(2, 3);
@@ -57,8 +150,6 @@ namespace Server.Mobiles
 
 		public override int Bones => Utility.RandomMinMax(2, 3);
 		public override BoneType BoneType => BoneType.Arachnide;
-
-
 		public override FoodType FavoriteFood => FoodType.Meat;
         public override PackInstinct PackInstinct => PackInstinct.Arachnid;
         public override Poison PoisonImmune => Poison.Greater;
