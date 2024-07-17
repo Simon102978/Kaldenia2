8455c30d7e9fb371a5dd5f4b20e5f45aef7375ab
Simon102978 <mitaniel@hotmail.com>
2024-07-17 18:22:25 -0400

    Correction du scorpion qui attaquait pas

--- Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
+++ Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
@@ -14,7 +14,7 @@ namespace Server.Mobiles
 
         [Constructable]
         public Scorpion()
-            : base(AIType.AI_Melee, FightMode.Closest, 10, 2, 0.2, 0.4)
+            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
             Name = "un scorpion";
             Body = 48;
