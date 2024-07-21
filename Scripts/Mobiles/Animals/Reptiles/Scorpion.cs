d420952067b3c372f34e93ebeb22032dfb8b94d6
Simon102978 <mitaniel@hotmail.com>
2024-07-21 16:46:49 -0400

    Le scorpion du désert va avoir les memes stats et aptitudes que le scorpion de présent. Le scorpion normal va retourner sous sa forme sans hidding qui avait avant.

--- Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
+++ Scripts/Mobiles/Animals/Reptiles/Scorpion.cs
@@ -1,17 +1,10 @@
-using System;
 using Server.Items;
-using Server;
 
 namespace Server.Mobiles
 {
-    
     [CorpseName("le corps d'un scorpion")]
     public class Scorpion : BaseCreature
     {
-      	public override bool CanStealth => true;
-
-        public DateTime LastFreeze;
-
         [Constructable]
         public Scorpion()
             : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
@@ -38,109 +31,23 @@ namespace Server.Mobiles
             SetResistance(ResistanceType.Poison, 40, 50);
             SetResistance(ResistanceType.Energy, 10, 15);
 
-            SetSkill(SkillName.Poisoning, 60.1, 80.0);
+            SetSkill(SkillName.Poisoning, 80.1, 100.0);
             SetSkill(SkillName.MagicResist, 30.1, 35.0);
             SetSkill(SkillName.Tactics, 60.3, 75.0);
             SetSkill(SkillName.Wrestling, 50.3, 65.0);
-            SetSkill(SkillName.Hiding,70.0, 100.0);
 
             Fame = 2000;
             Karma = -2000;
 
             Tamable = true;
-            ControlSlots = 2;
-            MinTameSkill = 77.1;
+            ControlSlots = 1;
+            MinTameSkill = 47.1;
         }
 
         public Scorpion(Serial serial)
             : base(serial)
         {
         }
-
-        public override void OnThink()
-        {
-
-            if (!Alive || Deleted)
-            {
-                return;
-            }
-
-            if (!Hidden)
-            {
-                double chance = 0.10;
-
-                if (Hits < 20)
-                {
-                    chance = 0.1;
-                }
-
-                if (Poisoned)
-                {
-                    chance = 0.01;
-                }
-
-                if (Utility.RandomDouble() < chance)
-                {
-                    HideSelf();
-                }
-                base.OnThink();
-            }
-        }
-
-        public override void OnDamage(int amount, Mobile from, bool willKill)
-        {
-            RevealingAction();
-            base.OnDamage(amount, from, willKill);
-
-        }
-
-
-
-		public override void AlterMeleeDamageTo(Mobile to, ref int damage)
-		{
-             double chance = 0.05;
-
-             if ( 3 < to.GetDistanceToSqrt(this.Location))
-             {
-                chance += 0.20; 
-             }
-
-            if ((Hidden || Utility.RandomDouble() < chance ) && LastFreeze < DateTime.UtcNow )
-            {
-                to.Freeze(TimeSpan.FromSeconds(3));
-                to.Emote("*Le scorpion vous paralyse.*");
-
-                 LastFreeze = DateTime.UtcNow + TimeSpan.FromSeconds(Utility.RandomMinMax(6, 9));
-            }
-
-			
-			base.AlterMeleeDamageTo(to, ref damage);
-		}
-
-
-
-
-        public override void OnDamagedBySpell(Mobile from)
-        {
-            RevealingAction();
-            base.OnDamagedBySpell(from);
-        }
-
-
-        private void HideSelf()
-        {
-            if (Core.TickCount >= NextSkillTime)
-            {
-                Effects.SendLocationParticles(
-                    EffectItem.Create(Location, Map, EffectItem.DefaultDuration), 0x3728, 10, 10, 2023);
-
-                PlaySound(0x22F);
-                Hidden = true;
-
-                UseSkill(SkillName.Hiding);
-            }
-        }
-
 
         public override int Meat => Utility.RandomMinMax(2, 3);
 
@@ -150,10 +57,12 @@ namespace Server.Mobiles
 
 		public override int Bones => Utility.RandomMinMax(2, 3);
 		public override BoneType BoneType => BoneType.Arachnide;
+
+
 		public override FoodType FavoriteFood => FoodType.Meat;
         public override PackInstinct PackInstinct => PackInstinct.Arachnid;
         public override Poison PoisonImmune => Poison.Greater;
-        public override Poison HitPoison => (0.8 >= Utility.RandomDouble() ? Poison.Regular : Poison.Greater);
+        public override Poison HitPoison => (0.8 >= Utility.RandomDouble() ? Poison.Greater : Poison.Deadly);
         public override void GenerateLoot()
         {
             AddLoot(LootPack.Meager);
--- Scripts/Mobiles/Normal/DesertScorpion.cs
+++ Scripts/Mobiles/Normal/DesertScorpion.cs
@@ -1,53 +1,146 @@
+using System;
+using Server.Items;
+
 namespace Server.Mobiles
 {
-    [CorpseName("le corps d'un scorpion du désert")]
+    [CorpseName("le corps d'un scorpion du dÃ©sert")]
     public class DesertScorpion : BaseCreature
     {
+        public override bool CanStealth => true;
+
+        public DateTime LastFreeze;
         [Constructable]
         public DesertScorpion()
             : base(AIType.AI_Melee, FightMode.Closest, 10, 1, .2, .4)
         {
-            Name = "un scorpion du désert";
+            Name = "un scorpion du dÃ©sert";
             Body = 0x2CD;
             BaseSoundID = 397;
 
-            SetStr(600, 700);
-            SetDex(120, 128);
-            SetInt(150, 200);
+            SetStr(73, 115);
+            SetDex(76, 95);
+            SetInt(16, 30);
 
-            SetDamage(15, 25);
+            SetHits(50, 63);
+            SetMana(0);
 
-            SetHits(350, 400);
-
-            SetResistance(ResistanceType.Physical, 30, 40);
-            SetResistance(ResistanceType.Fire, 50, 60);
-            SetResistance(ResistanceType.Cold, 10, 20);
-            SetResistance(ResistanceType.Poison, 70, 80);
-            SetResistance(ResistanceType.Energy, 40, 50);
+            SetDamage(5, 10);
 
             SetDamageType(ResistanceType.Physical, 60);
-            SetDamageType(ResistanceType.Physical, 40);
+            SetDamageType(ResistanceType.Poison, 40);
 
-            SetSkill(SkillName.MagicResist, 60, 70);
-            SetSkill(SkillName.Tactics, 80, 90);
-            SetSkill(SkillName.Wrestling, 50, 60);
-            SetSkill(SkillName.Poisoning, 110, 120);
+            SetResistance(ResistanceType.Physical, 20, 25);
+            SetResistance(ResistanceType.Fire, 10, 15);
+            SetResistance(ResistanceType.Cold, 20, 25);
+            SetResistance(ResistanceType.Poison, 40, 50);
+            SetResistance(ResistanceType.Energy, 10, 15);
 
-            Fame = 8100;
-            Karma = -8100;
+            SetSkill(SkillName.Poisoning, 60.1, 80.0);
+            SetSkill(SkillName.MagicResist, 30.1, 35.0);
+            SetSkill(SkillName.Tactics, 60.3, 75.0);
+            SetSkill(SkillName.Wrestling, 50.3, 65.0);
+            SetSkill(SkillName.Hiding,70.0, 100.0);
+
+            Fame = 2000;
+            Karma = -2000;
+
         }
 
         public override void GenerateLoot()
         {
-            AddLoot(LootPack.FilthyRich, 1);
-			AddLoot(LootPack.Others, Utility.RandomMinMax(1, 2));
+             AddLoot(LootPack.Meager);
+            AddLoot(LootPack.LootItem<LesserPoisonPotion>(true));
+			AddLoot(LootPack.Others, Utility.RandomMinMax(0, 2));
 
 		}
+        public override void OnThink()
+        {
 
-		public override Poison HitPoison => Poison.Lethal;
-        public override Poison PoisonImmune => Poison.Lethal;
-        public override int Meat => 3;
-        public override int TreasureMapLevel => 1;
+            if (!Alive || Deleted)
+            {
+                return;
+            }
+
+            if (!Hidden)
+            {
+                double chance = 0.10;
+
+                if (Hits < 20)
+                {
+                    chance = 0.1;
+                }
+
+                if (Poisoned)
+                {
+                    chance = 0.01;
+                }
+
+                if (Utility.RandomDouble() < chance)
+                {
+                    HideSelf();
+                }
+                base.OnThink();
+            }
+        }
+
+
+
+        public override void OnDamagedBySpell(Mobile from)
+        {
+            RevealingAction();
+            base.OnDamagedBySpell(from);
+        }
+
+
+        private void HideSelf()
+        {
+            if (Core.TickCount >= NextSkillTime)
+            {
+                Effects.SendLocationParticles(
+                    EffectItem.Create(Location, Map, EffectItem.DefaultDuration), 0x3728, 10, 10, 2023);
+
+                PlaySound(0x22F);
+                Hidden = true;
+
+                UseSkill(SkillName.Hiding);
+            }
+        }
+
+
+
+		public override void AlterMeleeDamageTo(Mobile to, ref int damage)
+		{
+             double chance = 0.05;
+
+             if ( 3 < to.GetDistanceToSqrt(this.Location))
+             {
+                chance += 0.20; 
+             }
+
+            if ((Hidden || Utility.RandomDouble() < chance ) && LastFreeze < DateTime.UtcNow )
+            {
+                to.Freeze(TimeSpan.FromSeconds(3));
+                to.Emote("*Le scorpion vous paralyse.*");
+
+                 LastFreeze = DateTime.UtcNow + TimeSpan.FromSeconds(Utility.RandomMinMax(6, 9));
+            }
+
+			
+			base.AlterMeleeDamageTo(to, ref damage);
+		}
+
+
+        public override int Meat => Utility.RandomMinMax(2, 3);
+
+		public override int Hides => Utility.RandomMinMax(2, 3);
+		public override HideType HideType => HideType.Arachnide;
+
+		public override int Bones => Utility.RandomMinMax(2, 3);
+		public override BoneType BoneType => BoneType.Arachnide;
+		public override FoodType FavoriteFood => FoodType.Meat;
+        public override PackInstinct PackInstinct => PackInstinct.Arachnid;
+        public override Poison PoisonImmune => Poison.Greater;
+        public override Poison HitPoison => (0.8 >= Utility.RandomDouble() ? Poison.Regular : Poison.Greater);
 
         public DesertScorpion(Serial serial)
             : base(serial)
