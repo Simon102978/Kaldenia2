b91e7a823f927d973a5cf7533a8c3098ee5bd93a
Simon102978 <mitaniel@hotmail.com>
2024-08-15 17:34:19 -0400

    Ajout d'un tableau pour le nombre de morts.

---
+++ Scripts/Custom/Commande/HallOfDeath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Server;
+using Server.Mobiles;
+using Server.Targeting;
+using Server.Commands;
+using Server.Gumps;
+using Server.Accounting;
+
+namespace Server.Scripts.Commands
+{
+    public class HallOfDeath
+	{
+        public static void Initialize()
+        {
+            CommandSystem.Register("HallOfDeath", AccessLevel.GameMaster, new CommandEventHandler(HallOfDeath_OnCommand));
+        }
+
+        public static void HallOfDeath_OnCommand(CommandEventArgs e)
+        {
+			Mobile from = e.Mobile;
+
+			if (from is CustomPlayerMobile cp)
+					from.SendGump(new HallOfDeathGump(cp,new System.Collections.Generic.Dictionary<Mobile, int>(), 0));
+
+
+
+        }
+
+
+    }
+}
